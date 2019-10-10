273d79c5f7a98bb35e14d78706910bb40f899861
mahato861 <55026761+mahato861@users.noreply.github.com>
2019-10-09 21:53:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.send('Nodejs for github for testing lab');
 });
 
-app.listen(process.env.PORT || 6000);
+app.listen(process.env.PORT || 5000);
 
 module.exports = app;
